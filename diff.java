37d9ab59fe200bdb593fa142a299187305459753
Sandeep Kumar <Sandeepkumar50220@gmail.com>
2021-06-21 12:11:35 +0530

    add diff file

---
+++ diff.java
@@ -0,0 +1,9 @@
+class diff{
+
+    public static void main (String [] args ){
+
+        int a=9;
+        int b =10;
+        System.out.println(b-a);
+    }
+}
--- example.java
+++ example.java
@@ -5,6 +5,7 @@ public static void  main (String [] args ){
    int b=10;
 
     System.out.println(a+b);
+    System.out.println(a-b);
 }
 
 }
